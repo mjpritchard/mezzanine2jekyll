19288a01c9be5eb6cc104e30909418980b3bc452
Matt Pritchard <matt.pritchard@stfc.ac.uk>
2023-10-11 13:38:53 +0100

    nearly there, image download works, check size

--- mezzanine2jekyll/management/commands/mezzanine2hugo.py
+++ mezzanine2jekyll/management/commands/mezzanine2hugo.py
@@ -3,44 +3,73 @@ from mezzanine.blog.models import BlogPost
 import os
 import re
 import markdownify
+from urllib.request import urlretrieve
+from urllib.error import HTTPError
 
 class Command(BaseCommand):
     help = 'Export Mezzanine blog posts as Hugo markdown files'
 
     def add_arguments(self, parser):
-        parser.add_argument('output_dir', help='Where to put the outputted Hugo markdown files')
+        parser.add_argument('output_dir', help='Where to put the outputted Hugo markdown files (relative path to local dir)')
         parser.add_argument('limit', help='how many blog posts to export (in reverse date order)')
         parser.add_argument('base_url', help='base url for blog posts e.g. /blog (no http(s):// or trailing slash)')
+        parser.add_argument('media_prefix', help="prefix url from which to fetch media, no trailing slash e.g. https://www.ceda.ac.uk")
+        parser.add_argument('media_dir', help="where to put the downloaded media files (relative path to local dir)")
         #parser.add_argument('end_date', help='end date for export (publish_date of post)')
 
     def handle(self, *args, **options):
-        output_dir = options['output_dir']
         limit = int(options['limit'])
-        base_url = str(options['base_url'])
-        #start_date = options['start_date']
-        #end_date = options['end_date']
 
-        STATUS_PUBLISHED = 2
+        for post in BlogPost.objects.published()[:limit]: #, publish_date = start_date):
 
-        for post in BlogPost.objects.filter(status=STATUS_PUBLISHED)[:limit]: #, publish_date = start_date):
-        
             tags = ['news',]
             for kw in post.keywords.all():
                 tags.append(str(kw))
 
-            url = '/'.join([base_url, post.slug])
+            url = '/'.join([options['base_url'], post.slug])
             aliases = []
             aliases.append(url)
 
-            print(aliases)
+            try:
+                print(post.slug, "featured image: " + post.featured_image.url)
 
-            #print(post.publish_date, post.title, len(tags), tags)
+                # fetch the image to a local directory
+                dest_path = os.path.join(options['media_dir'], post.slug)
+                print("2. media_prefix = ", options['media_prefix'])
+                source_url = options['media_prefix'] + post.featured_image.url
+
+                print("SOURCE: ", source_url)
+                
+                # Save featured image locally & return its new location
+                if os.path.exists(dest_path):
+                    print("EXISTS")
+                else:
+                    print("MAKEDIRS")
+                    os.makedirs(dest_path)
+
+                source_filename = os.path.basename(post.featured_image.url)
+                dest_file = os.path.join(dest_path, source_filename)
+
+                try:
+                    print("Trying download of ",source_url ,"to", dest_file)
+                    urlretrieve(source_url, dest_file)
+                except:
+                    print("Download failed")
+
+                print("DEST:", dest_file)
+                thumbnail = dest_file
+
+            except ValueError:
+                thumbnail=""
+
+            #TODO check image size options
 
             header = {
                 'title': post.title.replace(':', '').replace('"','').replace('\r\n',' '),
                 'date': post.publish_date,
                 'tags': tags,
-                'aliases': aliases
+                'aliases': aliases,
+                'thumbnail': thumbnail
             }
 
             filename = '{d.year:02}-{d.month:02}-{d.day:02}-{slug}.md'.format(
@@ -51,11 +80,14 @@ class Command(BaseCommand):
                 md_content = markdownify.markdownify(post.content)
 
                 # Write out the file
-                with open(os.path.join(output_dir, filename), 'w') as fp:
+                with open(os.path.join(options['output_dir'], filename), 'w') as fp:
                     fp.write('---' + os.linesep)
                     for k, v in header.items():
                         fp.write('%s: %s%s' % (k, v, os.linesep))
                     fp.write('---' + os.linesep)
+
+                    #TODO check for any other images in the text?
+
                     fp.write(md_content)
 
             except TypeError:
