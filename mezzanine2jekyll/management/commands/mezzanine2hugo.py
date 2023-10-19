b9332fee0906ef60cf08bfd93e4764b82c3e9580
Matt Pritchard <matt.pritchard@stfc.ac.uk>
2023-10-19 08:15:22 +0100

    hacky fixes but now works

--- mezzanine2jekyll/management/commands/mezzanine2hugo.py
+++ mezzanine2jekyll/management/commands/mezzanine2hugo.py
@@ -5,6 +5,7 @@ import re
 import markdownify
 from urllib.request import urlretrieve
 from urllib.error import HTTPError
+from urllib.parse import quote
 
 class Command(BaseCommand):
     help = 'Export Mezzanine blog posts as Hugo markdown files'
@@ -19,20 +20,27 @@ class Command(BaseCommand):
     
     def save_and_replace_image_link(self, m):
         g = list(m.groups())
-        print("SUB: SELF.FILENAME_BASE: ", self.filename_base)
+        print("SUB: SELF.FILENAME_BASE: ", self.filename_base, self.year)
         caption = g[0] # pass thro unchanged
         url = g[1].replace('/static', '')
         url = self.media_prefix + url # where to fetch from
         filename = os.path.basename(url) # just the filename part
-        save_path = os.path.join("img/news", self.filename_base) # make the path inc the date+slug dir string
-        save_filename = os.path.join(save_path, filename) 
-        if not os.path.exists(save_path):
-            os.makedirs(save_path)
+        save_path = "/img/news/" + self.year + "/" + self.filename_base # make the path inc the date+slug dir string
+        save_filename = os.path.join(save_path, filename)
+        print("MEDIA_DIR = ", self.media_dir)
+        print("SAVE_FILENAME:", save_filename)
+        real_save_path = self.media_dir + save_path
+        print("SUB: REAL_SAVE_PATH = ", real_save_path)
+        real_save_filename = self.media_dir + save_filename
+        if not os.path.exists(real_save_path):
+            print("Making path :", real_save_path )
+            os.makedirs(real_save_path)
         try:
-            print("RETRIEVE: ", url, save_filename)
-            urlretrieve(url, save_filename)
+            print("SUB: RETRIEVE: ", url, real_save_filename)
+            url = quote(url, safe="%/:=&?~#+!$,;'@()*[]")
+            urlretrieve(url, real_save_filename)
         except:
-            print("Error downloading image ", url, " for ", save_path)
+            print("Error downloading image ", url, " for ", real_save_path, "\n")
 
         shortcode = '{{< image src="{}"  caption="{}" class="rounded" >}}'.format(
             save_filename,
@@ -44,10 +52,13 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         limit = int(options['limit'])
         self.media_prefix = options['media_prefix']
+        self.media_dir = options['media_dir']
+
 
         for post in BlogPost.objects.published()[:limit]: #, publish_date = start_date):
             self.filename_base = '{d.year:02}-{d.month:02}-{d.day:02}-{slug}'.format(
                 d=post.publish_date, slug=post.slug)
+            self.year = post.publish_date.strftime("%Y")
             tags = ['news',]
             for kw in post.keywords.all():
                 tags.append(str(kw))
@@ -58,26 +69,30 @@ class Command(BaseCommand):
 
             try:
                 print(post.slug, "featured image: " + post.featured_image.url)
-
+                print("YEAR: ", self.year)
                 # fetch the image to a local directory
-                self.dest_path = os.path.join(options['media_dir'], self.filename_base)
+                self.dest_path = os.path.join("img/news", self.year, self.filename_base)
+                self.real_dest_path = os.path.join(options['media_dir'], self.dest_path)
                 source_url = options['media_prefix'] + post.featured_image.url
                 
                 # Save featured image locally & return its new location
-                if not os.path.exists(self.dest_path):
-                    os.makedirs(self.dest_path)
+                if not os.path.exists(self.real_dest_path):
+                    print("Making path :", self.real_dest_path )
+                    os.makedirs(self.real_dest_path)
 
                 source_filename = os.path.basename(post.featured_image.url)
-                dest_file = os.path.join(options["media_dir"], self.filename_base, source_filename)
+                dest_file = os.path.join(self.dest_path, source_filename)
+                real_dest_file = os.path.join(self.real_dest_path, source_filename)
 
                 try:
-                    print("Trying download of ",source_url ,"to", dest_file)
-                    urlretrieve(source_url, dest_file)
+                    print("Trying download of ",source_url ,"to", real_dest_file)
+                    url = quote(url, safe="%/:=&?~#+!$,;'@()*[]")
+                    urlretrieve(source_url, real_dest_file)
                 except:
                     print("Download failed")
 
                 print("DEST:", dest_file)
-                thumbnail = dest_file
+                thumbnail = "/" + dest_file
                 icon = ""
 
             except ValueError:
@@ -103,7 +118,11 @@ class Command(BaseCommand):
                 md_content = markdownify.markdownify(post.content)
 
                 # Write out the file
-                with open(os.path.join(options['output_dir'], filename), 'w') as fp:
+                output_dir = os.path.join(options['output_dir'], self.year)
+                if not os.path.exists(output_dir):
+                    os.makedirs(output_dir)
+
+                with open(os.path.join(output_dir, filename), 'w') as fp:
                     fp.write('---' + os.linesep)
                     for k, v in header.items():
                         fp.write('%s: %s%s' % (k, v, os.linesep))
@@ -117,6 +136,7 @@ class Command(BaseCommand):
 
                     fp.write(new_content)
 
-            except TypeError:
+            except TypeError as e:
 
-                print("content for post",post.slug, " not valid")
+                print("ERROR: content for post",post.slug, " not valid.\n", e)
+
